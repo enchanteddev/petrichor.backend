cd2a339dd2256b9b503c7ecf6e0eb9de4db897e9
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2023-11-16 21:14:21 +0530

    some tests

--- app/tests.py
+++ app/tests.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+
+class BasicAccount(TestCase):
+    def create(self) -> None:
+        self.client.post('/api/register/', data={
+            'username': 'hey',
+            'email': 'hey@hey.in',
+            'password': 'hey',
+            'phone': '1213131',
+            'college': 'hey',
+            'year': '3',
+        })
