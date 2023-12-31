83105df6fa24d64f27adee34b768ffc5fb01f33a
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2023-12-31 14:56:30 +0530

    fixed more packaegs

--- userapi/settings.py
+++ userapi/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 
 DEBUG = False
-if DEBUG: load_dotenv()
+if DEBUG: 
+    from dotenv import load_dotenv
+    load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
