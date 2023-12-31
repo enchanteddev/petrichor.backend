805c31389d910d0cd3ba102994d4f5d4e2d1af51
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2023-12-31 14:16:56 +0530

    fixes in settings

--- userapi/settings.py
+++ userapi/settings.py
@@ -174,14 +174,13 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
-if not DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PWD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT')
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PWD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
+}
