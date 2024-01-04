3dca7b4f43c40300a0c443af8f15983bda86d1dd
alonot <priyanshu27092004@gmail.com>
2024-01-05 00:22:09 +0530

    error handling

--- app/views.py
+++ app/views.py
@@ -22,91 +22,105 @@ def home(request):
 @api_view(['POST'])
 def signup(request):
     if request.method == 'POST':
-        data = request.data
-        username = data['username'].strip()
-        email = data['email']
-        pass1 = data['password']
-        phone = data['phone']
-        insti_name = data['college']
-        gradyear = data['gradyear']
-        insti_type = data['institype']
-        stream = data['stream']
-        
-        if User.objects.filter(email=email).first():
-            return Response({
-                'status': 404,
-                "registered": False,
-                'message': "Email already registered",
-                "username": username
-            })
-
-
-        else:
-            try:
-                new_user = User(username=email)
-                new_user.set_password(pass1)
-                new_user.is_active = True
-                new_user.save()
-            except IntegrityError:
-                return r500('Email already exists')
+        try:
+            data = request.data
+            username = data['username'].strip()
+            email = data['email']
+            pass1 = data['password']
+            phone = data['phone']
+            insti_name = data['college']
+            gradyear = data['gradyear']
+            insti_type = data['institype']
+            stream = data['stream']
             
-            try:
-
-                institute = Institute.objects.get_or_create(instiName=insti_name, institutionType=insti_type)[0]
-                # institute = Institute.objects.get(instiName=instituteID)
-
-                print(institute.pk)
-
-
-                user_profile = Profile.objects.create(username=username, 
-                                                    email=email,
-                                                    phone=phone,
-                                                    instituteID=institute.pk,
-                                                    gradYear=gradyear,
-                                                    stream=stream)
-                user_profile.save()
-
+            if User.objects.filter(email=email).first():
                 return Response({
-                    'status': 200,
-                    "registered": True,
-                    'message': "Success",
+                    'status': 404,
+                    "registered": False,
+                    'message': "Email already registered",
                     "username": username
                 })
-            except IntegrityError:
-                return r500("User already exists. Try something different.")
-            except:
-                r500("Something failed")
+
+
+            else:
+                try:
+                    new_user = User(username=email)
+                    new_user.set_password(pass1)
+                    new_user.is_active = True
+                    new_user.save()
+                except IntegrityError:
+                    # send_error_mail(inspect.stack()[0][3], request.data, e)  # Leave this commented otherwise every wrong login will send an error mail
+                    return r500('Email already exists')
+                
+                try:
+
+                    institute = Institute.objects.get_or_create(instiName=insti_name, institutionType=insti_type)[0]
+                    # institute = Institute.objects.get(instiName=instituteID)
+
+                    print(institute.pk)
+
+
+                    user_profile = Profile.objects.create(username=username, 
+                                                        email=email,
+                                                        phone=phone,
+                                                        instituteID=institute.pk,
+                                                        gradYear=gradyear,
+                                                        stream=stream)
+                    user_profile.save()
+
+                    return Response({
+                        'status': 200,
+                        "registered": True,
+                        'message': "Success",
+                        "username": username
+                    })
+                except IntegrityError:
+                    # send_error_mail(inspect.stack()[0][3], request.data, e)  # Leave this commented otherwise every wrong login will send an error mail
+
+                    return r500("User already exists. Try something different.")
+                except:
+                    send_error_mail(inspect.stack()[0][3], request.data, e)  
+                    r500("Something failed")
+                    
+        except Exception as e:
+            send_error_mail(inspect.stack()[0][3], data, e)
+            return r500("Something Bad Happened")
+
 
 
 @api_view(['POST'])
 def user_login(request):
     if request.method == 'POST':
+        try:
+            if request.data is None:
+                return r500("invalid form")
+            data=request.data
 
-        if request.data is None:
-            return r500("invalid form")
-        data=request.data
-
-        username = data['username'].strip()
-        print(username)
-        password = data['password']
-        print(password)
-        my_user = authenticate(username = username, password = password)
-        if my_user is not None:
-            login(request, my_user)
-            profile = Profile.objects.get(email=username)
-            token = (request.session.session_key)
-            print("OK")
+            username = data['username'].strip()
+            print(username)
+            password = data['password']
+            print(password)
+            my_user = authenticate(username = username, password = password)
+            if my_user is not None:
+                login(request, my_user)
+                profile = Profile.objects.get(email=username)
+                token = (request.session.session_key)
+                print("OK")
+                return Response({
+                    "ok": True,
+                    "username": profile.username,
+                    "token": token,
+                    "registrations": 0
+                })
+            print("Failed")
             return Response({
-                "ok": True,
-                "username": profile.username,
-                "token": token,
-                "registrations": 0
+                "message": "The username or password is incorrect",
+                "logged-in": "false"
             })
-        print("Failed")
-        return Response({
-            "message": "The username or password is incorrect",
-            "logged-in": "false"
-        })
+        except Exception as e:
+            send_error_mail(inspect.stack()[0][3], data, e)
+            return r500("Something Bad Happened")
+        
 
 @api_view(['POST'])
 def user_logout(request):
@@ -121,129 +135,145 @@ def user_logout(request):
 
 @api_view(['POST'])
 def getUserInfo(request):
-    print(request.data,"p")
-    user = get_user_from_session(request)
-    if user:
-        print("Passed",user,"p")
-        profileUser=Profile.objects.get(email=user)
-        if profileUser:
-            response={
-                "username":profileUser.username,
-                "email":profileUser.email,
-                "phone":profileUser.phone,
-                "gradyear":profileUser.gradYear,
-                "stream":profileUser.stream
-            }
-            instituteName=""
-            try:
-                instituteName=Institute.objects.get(pk=profileUser.instituteID).instiName
-            except ObjectDoesNotExist:
-                return r500("Oopss")
-            response["college"]=instituteName
+    try:
+        print(request.data,"p")
+        user = get_user_from_session(request)
+        if user:
+            print("Passed",user,"p")
+            profileUser=Profile.objects.get(email=user)
+            if profileUser:
+                response={
+                    "username":profileUser.username,
+                    "email":profileUser.email,
+                    "phone":profileUser.phone,
+                    "gradyear":profileUser.gradYear,
+                    "stream":profileUser.stream
+                }
+                instituteName=""
+                try:
+                    instituteName=Institute.objects.get(pk=profileUser.instituteID).instiName
+                except ObjectDoesNotExist:
+                    return r500("Oopss")
+                response["college"]=instituteName
 
-            events=[]
-            eventEntries=EventTable.objects.all()
-            for eventEntry in eventEntries:
-                if str(user) in eventEntry.emails:
-                    events.append({"eventId":eventEntry.eventId,"status":eventEntry.verified})
-            response["events"]=events
+                events=[]
+                eventEntries=EventTable.objects.all()
+                for eventEntry in eventEntries:
+                    if str(user) in eventEntry.emails:
+                        events.append({"eventId":eventEntry.eventId,"status":eventEntry.verified})
+                response["events"]=events
+                return Response({
+                    "status":200,
+                    "response":response
+                    })
+        else:
+            print("Auth failed")
+            # return Response({
+            #     "status":200,
+            #     "response":{
+            #         "username":"alonot",
+            #         "email":"csk1@gmail.com",
+            #         "phone":"1221211221",
+            #         "instituteID":"11222",
+            #         "college":"IIT PKD",
+            #         "gradyear":2029,
+            #         "stream":"Science",
+            #         "events":[{"eventId":"TP00","status":"Payment Verified"},
+            #                   {"eventId":"WP02","status":"Payment Pending"}]
+            #     }
+            # })
             return Response({
-                "status":200,
-                "response":response
-                })
-    else:
-        print("Auth failed")
-        # return Response({
-        #     "status":200,
-        #     "response":{
-        #         "username":"alonot",
-        #         "email":"csk1@gmail.com",
-        #         "phone":"1221211221",
-        #         "instituteID":"11222",
-        #         "college":"IIT PKD",
-        #         "gradyear":2029,
-        #         "stream":"Science",
-        #         "events":[{"eventId":"TP00","status":"Payment Verified"},
-        #                   {"eventId":"WP02","status":"Payment Pending"}]
-        #     }
-        # })
-        return Response({
-            "status":404,
-            "response":None
-        })
-
+                "status":404,
+                "response":None
+            })
+    except Exception as e:
+            send_error_mail(inspect.stack()[0][3], request.data, e)
+            return r500("Something Bad Happened")
 
 def get_user_from_session(request):
-    print(request.data)
     try:
-        session = Session.objects.get(session_key=request.data["token"])
-    except:
+        print(request.data)
+        try:
+            session = Session.objects.get(session_key=request.data["token"])
+        except:
+            return None
+        session_data = session.get_decoded()
+        print(session_data)
+        uid = session_data.get('_auth_user_id')
+        user = User.objects.get(id=uid)
+        if user:
+            return user
+        else:
+            print(request.session.session_key)
         return None
-    session_data = session.get_decoded()
-    print(session_data)
-    uid = session_data.get('_auth_user_id')
-    user = User.objects.get(id=uid)
-    if user:
-        return user
-    else:
-        print(request.session.session_key)
-        return None
+    except Exception as e:
+            send_error_mail(inspect.stack()[0][3], request.data, e)
+            return r500("Something Bad Happened")
+
 
 @api_view(['POST'])
 def whoami(request: Request):
-    user = get_user_from_session(request)
-    events=[]
-    if user is not None:
-        eventEntries=EventTable.objects.all()
-        for eventEntry in eventEntries:
-            if str(user) in eventEntry.emails:
-                events.append(eventEntry.eventId)
-    print({
-        'user': str(user) if user else None, # type: ignore
-        # 'user': str(user) if user else None , # type: ignore
-        'events': events,
-        'email': user.email #type: ignore
-    })
-    return Response({
-        'user': str(user) if user else None, # type: ignore
-        # 'user': str(user) if user else None , # type: ignore
-        'events': events,
-        'email': str(user) #type: ignore
-    })
-
+    try:
+        user = get_user_from_session(request)
+        events=[]
+        if user is not None:
+            eventEntries=EventTable.objects.all()
+            for eventEntry in eventEntries:
+                if str(user) in eventEntry.emails:
+                    events.append(eventEntry.eventId)
+        print({
+            'user': str(user) if user else None, # type: ignore
+            # 'user': str(user) if user else None , # type: ignore
+            'events': events,
+            'email': user.email #type: ignore
+        })
+        return Response({
+            'user': str(user) if user else None, # type: ignore
+            # 'user': str(user) if user else None , # type: ignore
+            'events': events,
+            'email': str(user) #type: ignore
+        })
+    except Exception as e:
+            send_error_mail(inspect.stack()[0][3], request.data, e)
+            return r500("Something Bad Happened")
+    
 
 # @login_required
 @api_view(['POST'])
 def apply_event_paid(request: Request):
-    data=request.data
-    if data is None:
-        return r500("invalid form")
     try:
-        user=get_user_from_session(request)
-        if user is None:
-            return r500('login')
-        user_email = user.username # type: ignore
-        participants = data['participants'] # type: ignore
-        event_id = data['eventId'].strip() # type: ignore
-        transactionId = data['transactionID'].strip() # type: ignore
+        data=request.data
+        if data is None:
+            return r500("invalid form")
+        try:
+            user=get_user_from_session(request)
+            if user is None:
+                return r500('login')
+            user_email = user.username # type: ignore
+            participants = data['participants'] # type: ignore
+            event_id = data['eventId'].strip() # type: ignore
+            transactionId = data['transactionID'].strip() # type: ignore
 
-    except KeyError:
-        print(data)
-        return r500("participants, eventid and transactionId required. send all.'")
-    try:
-        verified=False
-        if user_email.endswith("smail.iitpkd.ac.in"):
-            verified=True
-            transactionId="IIT Palakkad Student"
-            
-        eventTableObject = EventTable.objects.create(eventId=event_id,
-                                                    emails=EventTable.serialise_emails(participants), #type: ignore
-                                                    transactionId=transactionId,verified=verified)
-        eventTableObject.save()
-        return r200("Event applied")
-    except Exception:
-        return r500("Already Applied")
-
+        except KeyError:
+            print(data)
+            return r500("participants, eventid and transactionId required. send all.'")
+        try:
+            verified=False
+            if user_email.endswith("smail.iitpkd.ac.in"):
+                verified=True
+                transactionId="IIT Palakkad Student"
+                
+            eventTableObject = EventTable.objects.create(eventId=event_id,
+                                                        emails=EventTable.serialise_emails(participants), #type: ignore
+                                                        transactionId=transactionId,verified=verified)
+            eventTableObject.save()
+            return r200("Event applied")
+        except Exception:
+            return r500("Already Applied")
+    except Exception as e:
+            send_error_mail(inspect.stack()[0][3], data, e)
+            return r500("Something Bad Happened")
+    
 
 # @login_required
 @api_view(['POST'])
@@ -284,37 +314,41 @@ def apply_event_free(request):
 def send_error_mail(name, data, e):
     send_mail(f'Website Error in: {name}',
                 message= f'Exception: {e}\nData: {json.dumps(data)}',
-                recipient_list=['112201015@smail.iitpkd.ac.in'],
+                recipient_list=['112201015@smail.iitpkd.ac.in','112201020@smail.iitpkd.ac.in'],
                 from_email=settings.EMAIL_HOST_USER)
 
 # @login_required # limits the calls to this function ig
 @api_view(['POST'])
 def get_event_data(request):
-    data=request.data
-    print(data)
-    if data is None:
-        return r500("invalid form")
     try:
-        event_id = data["id"]
+        data=request.data
         print(data)
-    except KeyError:
-        return r500("Send an eventID")
-    
-    event = Event.objects.filter(eventId = event_id).first()
-    if event is None:
+        if data is None:
+            return r500("invalid form")
+        try:
+            event_id = data["id"]
+            print(data)
+        except KeyError:
+            return r500("Send an eventID")
+        
+        event = Event.objects.filter(eventId = event_id).first()
+        if event is None:
+            return Response({
+            "name": "Its nothing",
+            "fee": 0,
+            "minMemeber": 0,
+            "maxMemeber": 0
+        })
+            return r500(f"Invalid Event ID = {event_id}")
         return Response({
-        "name": "Its nothing",
-        "fee": 0,
-        "minMemeber": 0,
-        "maxMemeber": 0
-    })
-        return r500(f"Invalid Event ID = {event_id}")
-    return Response({
-        "name": event.name,
-        "fee": event.fee,
-        "minMemeber": event.minMember,
-        "maxMemeber": event.maxMember
-    })
+            "name": event.name,
+            "fee": event.fee,
+            "minMemeber": event.minMember,
+            "maxMemeber": event.maxMember
+        })
+    except Exception as e:
+            send_error_mail(inspect.stack()[0][3], data, e)
+            return r500("Something Bad Happened")
 
 
 @api_view(['POST'])
@@ -341,6 +375,7 @@ def send_grievance(request: Request):
             })
 
     except Exception as e:
+        send_error_mail(inspect.stack()[0][3], data, e)
         return Response({
                 'status':400,
                 'success': False
